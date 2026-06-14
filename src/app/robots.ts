d6740c9541c239044893c9b7c7758a21e05eb05a
Alberto Landa <52215286+SirkoBot007@users.noreply.github.com>
2026-06-14 04:13:11 +0200

    sync: upload robots.ts from local

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,16 @@
 import { MetadataRoute } from 'next'
-import { SITE_URL } from '@/lib/constants'
+import { SITE_URL }      from '@/lib/constants'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: '*', allow: '/', disallow: ['/api/'] }],
-    sitemap: `${SITE_URL}/sitemap.xml`,
-    host: SITE_URL,
+    rules: [
+      {
+        userAgent: '*',
+        allow:     '/',
+        disallow:  ['/api/', '/_next/'],
+      },
+    ],
+    sitemap:  `${SITE_URL}/sitemap.xml`,
+    host:     SITE_URL,
   }
 }
